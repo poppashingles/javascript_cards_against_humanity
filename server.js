287dfb0d6e21734203f6e3d7ce3360cdc99389b1
Rob <robdwilliams85@gmail.com>
2018-01-04 12:48:28 +0000

    Added the mongo client connect code and added an initial bootstrap html template

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const MongoClient = require('mongodb').MongoClient
 server.use(parser.json())
 server.use(express.static('client/build'))
 server.use(parser.urlencoded({extended: true}))
+
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if(err) {
+    console.log(err)
+    return
+  }
+
+
+  server.listen(3000, function(){
+    console.log("Listening on port 3000")
+  })
+})
