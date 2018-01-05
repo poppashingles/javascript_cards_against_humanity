8eac7809f46b10c6f7ed7e039dba4db9825bd737
Rob <robdwilliams85@gmail.com>
2018-01-05 14:56:01 +0000

    .

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static('client/build'))
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
-  console.log('App running!');
+  console.log('App running!!');
 });
