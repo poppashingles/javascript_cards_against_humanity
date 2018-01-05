2319ccbd6e08a5496109105bfedefec8edf51d81
Rob <robdwilliams85@gmail.com>
2018-01-05 14:58:46 +0000

    binding to heroku port

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(bodyParser.json());
 app.use(express.static('client/build'))
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
   console.log('App running!!');
 });
