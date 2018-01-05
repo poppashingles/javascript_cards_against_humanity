cb4b556077fa429c8af41328bcd244e6bbc140da
Rob <robdwilliams85@gmail.com>
2018-01-05 14:50:58 +0000

    Refinement to the server.js file

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(express.static('client/build'))
 app.use(bodyParser.urlencoded({extended: true}));
-server.use(express.static('client/build'))
-
-app.use(require('./controllers/index'));
 
 app.listen(3000, function() {
   console.log('App running!');
