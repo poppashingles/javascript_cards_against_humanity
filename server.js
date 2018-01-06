8524d81eff0b495ed69c3180d8dddc217e0ab108
Rob <robdwilliams85@gmail.com>
2018-01-06 18:20:16 +0000

    Added the html elements for entering a username and amended the CSS to suit

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(bodyParser.json());
 app.use(express.static('client/build'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
-
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+http.listen(port, function() {
   console.log('App running!!');
 });
